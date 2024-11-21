4e833f99ef947d4f8ba25d7727f07644937a2018
Michael Omonedo <rukkyomonedo@gmail.com>
2024-11-21 13:06:05 +0100

    changes to the food section

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -26,32 +26,57 @@ const Landing = () => {
     );
   }
   return (
-    <div className={styles.page}>
-      <div className={handlee.className}>
-        <div className={styles["food-area"]}>
-          {data.map((food) => (
-            <div className={styles["food-area-table"]} key={food.idCategory}>
-              <div
-                onClick={() => {
-                  handleClick(food.idCategory);
-                }}
-                className={styles["food-area-cate"]}
-              >
-                {food.strCategory}
-              </div>
-              
-              {/* {selectedCategoryId === food.idCategory && ( */}
-                <div>
-                  <img src={food.strCategoryThumb} />
-                  <div>{food.strCategoryDescription}</div>
-                </div>
-              {/* )} */}
-            </div>
-          ))}
-        </div>
+    <div className={styles["food-area"]}>
+      <div className={styles["food-area-title"]}>
+        <span id={selectedCategoryId}>Beef</span>
+        <span id={selectedCategoryId}>Chicken</span>
+        <span id={selectedCategoryId}>Dessert</span>
+        <span id={selectedCategoryId}>Lamb</span>
+        <span id={selectedCategoryId}>Miscellaneous</span>
+        <span id={selectedCategoryId}>Pasta</span>
+        <span id={selectedCategoryId}>Pork</span>
+        <span id={selectedCategoryId}>Seafood</span>
+        <span id={selectedCategoryId}>Side</span>
+        <span id={selectedCategoryId}>Starter</span>
+        <span id={selectedCategoryId}>Vegan</span>
+        <span id={selectedCategoryId}>Vegetarian</span>
+        <span id={selectedCategoryId}>Breakfast</span>
+        <span id={selectedCategoryId}>Goat</span>
       </div>
+      {data.map((food) => (
+    
+        <div key={food.idCategory} className={styles["food-area-table"]}>
+          <span className={styles["food-area-meal"]}>{food.strCategory}</span>
+          <img src={food.strCategoryThumb} alt="Food image" />
+          <span>{food.strCategoryDescription}</span>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Landing;
+
+// <div className={handlee.className}>
+//      <div className={styles["food-area"]}>
+//      {data.map((food) => (
+//       <div className={styles["food-area-table"]} key={food.idCategory}>
+//         <div
+//           onClick={() => {
+//             handleClick(food.idCategory);
+//           }}
+//           className={styles["food-area-cate"]}
+//         >
+//          {food.strCategory}
+//        </div>
+//
+//        {/* {selectedCategoryId === food.idCategory && ( */}
+//          <div>
+//            <img src={food.strCategoryThumb} />
+//            <div>{food.strCategoryDescription}</div>
+//          </div>
+//        {/* )} */}
+//      </div>
+//    ))}
+//  </div>
+// </div>
